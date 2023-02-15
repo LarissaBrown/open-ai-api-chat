ffa55aa80cb0f659a6907b4ce5f36680a16f1487
Larissa Brown <larisa.brown.johnson@gmail.com>
2023-02-14 17:22:18 -0700

    main - commit for steve

--- client/src/assets/components/ChatMessage.jsx
+++ client/src/assets/components/ChatMessage.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
+
 export default function ChatMessage(props){
+
     return (
-    <div className="chatlog">
-        <div className="chat-message">
+    <div >
+        <div className='chat-message'>
         <div className="chat-message-center">
         <div className="message">{props.message}</div>
         </div>
         </div>
         <p className="date-stamp">8:30pm</p>
+        <div className='chat-openai'>
+        <div className="message-openai">{props.aimessage}</div>
+        <p className="date-stamp">8:30pm</p>
+        </div>
     </div>
     )
 }
